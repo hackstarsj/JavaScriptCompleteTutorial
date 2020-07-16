d1f81707a571649a762fbcbf7d80c7948e76b0ae
sanjeev kumar <sksanjeev362@gmail.com>
2020-07-16 18:19:42 +0530

    Added Calculator Log Using JavaScript Object and Array

--- calculator.js
+++ calculator.js
@@ -1,8 +1,12 @@
 var input_box=document.getElementById("calculation");
+var historyData=[];
+var expressionData="";
+var resultData="";
 
 function UserClickButton(input) {
     var oldinput=input_box.value;
     var newinput=oldinput+input;
+    expressionData=newinput;
     input_box.value=newinput;
 }
 function CalculateValue() {
@@ -10,8 +14,24 @@ function CalculateValue() {
     var input=input_box.value;
     input=input.replace('"',"").replace("'","");
     var result=eval(input);
+    resultData=result;
+    historyData.push({"expression":expressionData,"result":resultData});
+    showLogdata();
+    resultData="";
+    expressionData="";
     input_box.value=result;
 }
 function ClearData() {
     input_box.value="";
+}
+
+function showLogdata() {
+    var log=document.getElementById("history_log");
+    var string="";
+    for(var key in historyData){
+        string+=""+historyData[key]["expression"]+" = "+historyData[key]["result"]+"<br>";
+    }
+
+
+    log.innerHTML=string;
 }
